bc26f4fdb985213bf5c20bc2f7ed4c64eeac975a
Eyuel3113 <eyuelendale26@.com>
2025-12-20 11:39:39 +0300

    fixing build issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import './globals.css'
-import Navbar from '../components/Navbar'
-import Footer from '@/components/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-white text-black">
-        <Navbar />
-        <div className="">{children}</div> 
-       <Footer />
+      <body className="antialiased">
+        {children}
       </body>
-       
     </html>
   )
 }
